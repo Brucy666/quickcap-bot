84f46e810e9f168ea9d8978e9efafeee8c1ab785
Brucy666 <robleach16@icloud.com>
2025-08-14 08:56:53 +0100

    Update __init__.py

--- app/exchanges/__init__.py
+++ app/exchanges/__init__.py
@@ -1,2 +1,5 @@
 from .kucoin import KuCoinPublic
 from .mexc import MEXCPublic
+from .bybit import BybitSpotPublic, BybitPerpPublic
+from .binance import BinanceSpotPublic, BinancePerpPublic
+from .okx import OKXSpotPublic, OKXPerpPublic
